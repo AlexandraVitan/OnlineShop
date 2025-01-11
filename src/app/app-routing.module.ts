93949a914b567f0ac075374d6e039f80507fbcec
Alexandra Vitan <alexandravitan2015@gmail.com>
2025-01-11 22:15:21 +0200

    added new pages, and styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PerfumePageComponent } from './perfume-page/perfume-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { AboutComponent } from './about/about.component';
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'search/:searchTerm', component: HomeComponent },
   { path: 'tag/:tag', component: HomeComponent },
   { path: 'perfume/:id', component: PerfumePageComponent },
   { path: 'cart-page', component: CartPageComponent },
+  { path: 'welcome-page', component: WelcomePageComponent },
+  { path: 'about', component: AboutComponent },
+
 ];
 
 @NgModule({
--- src/app/shared/models/perfume.ts
+++ src/app/shared/models/perfume.ts
@@ -2,7 +2,7 @@ export class Perfume{
     id!:number;
     name!:string;
     price!:number;
-    description!: string;
+    notes!: string;
     tags?:string[];
     favorite:boolean = false;
     imageUrl!:string;
